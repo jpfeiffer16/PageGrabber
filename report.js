0f169e1de5c5f35706810085c047fb9eb25008b7
jpfeiffer16 <jpfeiffer@bluemodus.com>
2016-11-25 18:12:18 -0700

    Getting reporting system working

--- report.js
+++ report.js
@@ -2,20 +2,28 @@ const program = require('commander');
 const fs = require('fs');
 const path = require('path');
 
+let reportsPath = path.normalize(path.join(__dirname, 'reports'));
+
 program
   .version(require('./package.json').version)
-  .option('-r --report [string]', 'Report to generate');
-  
-program.on('--help', () => {
-    console.log('Custom help');
-    console.log(path.normalize(path.join(__dirname, 'reports')));
-    //TODO: Generate list of reports and their info hre.  
-    fs.readdir(path.normalize(path.join(__dirname, 'reports')), (err, files) => {
-      console.log(files);
-      if (err) throw err;
-      files.forEach((file) => {
-        // console.log(require(`./repors/${ file }`).info);
-      });
+  .option('-r --report [string]', 'Report to generate')
+  .on('--help', () => {
+    let files = fs.readdirSync(reportsPath);
+    if (files.length == 0) {
+      console.log('There are no reports to run');
+      return;
+    }
+    console.log('Reports: ');
+    files.forEach((file) => {
+      console.log(`    ${ file.replace('.js', '') } : ${ require(path.join(reportsPath, file)).info }`);
     });
   })
   .parse(process.argv);
+
+//Sanity Checks
+if (!program.report) {
+  console.warn('Must include a report to generate. Use --help to see usage options');
+  process.exit(1);
+}
+
+require(path.join(reportsPath, program.report)).gen();
--- reports/httpoccurences.js
+++ reports/httpoccurences.js
@@ -1,6 +1,6 @@
 module.exports = {
   info: 'Generate a report that tracks the number and line numbers of non-secure links',
   gen: (Models) => {
-    
+    console.log('fist report being called'); 
   }
 }
