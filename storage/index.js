c63b6317ed34de61d24ccae37611b8a7b37fc5ce
Joe Pfeiffer <jpfeiffer@bluemodus.com>
2016-11-26 01:19:42 -0700

    Fixing bug in db location

--- storage/index.js
+++ storage/index.js
@@ -4,24 +4,13 @@ module.exports = function(cb) {
   const sequelize = new Sequelize({
     logging: false,
     dialect: 'sqlite',
-    storage: require('path').normalize(require('path').join(__dirname, 'data.sqlite'))
+    storage: require('path').normalize(require('path').join(__dirname, '../data.sqlite'))
   });
-  console.log(require('path').normalize(require('path').join(__dirname, 'data.sqlite')));
+
   //Define Modules
   let Models = {};
   Models.Result = require('./result')(sequelize, Sequelize);
-
-  //Temp
-  // let Result = require('./result')(sequelize, Sequelize);
-  // console.log('Result:');
-  // // console.dir(Result, { depth: 2 });
-  // require('./result')(sequelize, Sequelize)
-  //   .findAll()
-  //   .then((dbresults) => {
-  //     console.log('Found all');
-  //     console.log(dbresults);
-  //   });
-
+  
   //Tie logic to on db setup
   sequelize.sync().then(() => { 
     cb(Models);
